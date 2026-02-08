de83c1b529924a76737c09fb48b313015b1eb803
mix0love <parad5451@gmail.com>
2026-02-08 19:17:11 +0500

    Update site data 2026-02-08 19:17

--- js/util.js
+++ js/util.js
@@ -10,7 +10,7 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 3 });
+    return num.toLocaleString(undefined, { maximumFractionDigits: 3 });
 }
 
 export function getThumbnailFromId(id) {
